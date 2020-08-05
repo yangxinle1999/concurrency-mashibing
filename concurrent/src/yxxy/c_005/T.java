34d2648514b4eb9e8378b1b7a7c6eed0f034f59b
yangxinle1999 <2842611637@qq.com>
2020-08-05 22:33:21 +0800

    被synchronized修饰的部分是原子操作

--- concurrent/src/yxxy/c_005/T.java
+++ concurrent/src/yxxy/c_005/T.java
@@ -1,14 +1,26 @@
 package yxxy.c_005;
 
-
+//被synchronized修饰的部分是原子操作
 class T implements Runnable {
 
     private int count=10;
 
     @Override
     public /*synchronized*/ void run() {
-        count--;
+        count--; //不是原子操作
         System.out.println(Thread.currentThread().getName()+" count = "+count);
+//        Thread0 count = 9
+//        Thread4 count = 5
+//        Thread3 count = 6
+//        Thread2 count = 7
+//        Thread1 count = 8
+
+        //加上synchronized之后，一定是：
+//        Thread0 count = 9
+//        Thread1 count = 8
+//        Thread2 count = 7
+//        Thread3 count = 6
+//        Thread4 count = 5
     }
 
     public static void main(String[] args) {
