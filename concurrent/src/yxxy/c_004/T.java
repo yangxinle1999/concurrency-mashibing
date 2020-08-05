434834b522c72f011e0a0c4e6b64d4da661831dc
yangxinle1999 <2842611637@qq.com>
2020-08-05 22:26:07 +0800

    静态方法锁的是Class，一个类加载到JVM之后只会有一个Class类

--- concurrent/src/yxxy/c_004/T.java
+++ concurrent/src/yxxy/c_004/T.java
@@ -1,6 +1,6 @@
 package yxxy.c_004;
 
-
+//静态方法锁的是Class，一个类加载到JVM之后只会有一个Class类
 class T {
     private static int count=10;
 
@@ -10,7 +10,7 @@ class T {
     }
 
     public static void mm(){
-        synchronized (T.class){ //这里写synchronized(this)是否可以?
+        synchronized (T.class){ //这里写synchronized(this)是否可以?显然不可以，因为静态方法不属于某个对象，只属于这个类
             count--;
         }
     }
